b89223bd6777cbff1ef54558f9f4c6453850c9d8
FEGysin <fegysin@gmail.com>
2023-01-09 20:44:27 -0300

    Primer Desafio

---
+++ index.js
@@ -0,0 +1,81 @@
+class ProductMannager {
+  constructor() {
+    this.products = [];
+  }
+
+  addProduct = (producto, precio, imgPath, intcode, stock) => {
+    const product = {
+      id: 0,
+      title: producto,
+      price: precio,
+      thumbnail: imgPath,
+      code: intcode,
+      stock: stock,
+    };
+    if (this.products.length === 0) {
+      product.id = 1;
+    } else {
+      product.id = this.products[this.products.length - 1].id + 1;
+    }
+
+    this.products.push(product);
+  };
+  getProducts = () => {
+    return this.products;
+  };
+  getProductById = (pos) => {
+    const res = this.products.filter((product) => product.id === pos);
+    // console.log(res);
+    if (res.length === 0) {
+      return "Not Found";
+    } else {
+      return res;
+    }
+  };
+}
+const PM = new ProductMannager();
+
+addProducts();
+console.log("Listado de Productos");
+console.log(PM.getProducts());
+console.log("");
+console.log("");
+console.log(`Producto id=1`);
+console.log(PM.getProductById(1));
+console.log(`Producto id=5`);
+console.log(PM.getProductById(5));
+console.log(`Producto id=2`);
+console.log(PM.getProductById(2));
+console.log(`Producto id=4`);
+console.log(PM.getProductById(4));
+console.log(`Producto id=3`);
+console.log(PM.getProductById(3));
+console.log(`Producto id=6`);
+console.log(PM.getProductById(6));
+
+function addProducts() {
+  // title, price, thumbnail, stock
+  PM.addProduct("Madera de Pino", 200, "./img/MaderaPino.jpg", `md-Pino`, 20);
+  PM.addProduct(
+    "Madera de Quebracho",
+    500,
+    "./img/QuebrachoColorado.jpg",
+    "md-QuebCol",
+    5
+  );
+  PM.addProduct(
+    "Madera de Espinillo",
+    350,
+    "./img/MaderaEspinillo.jpg",
+    "md-Espinillo",
+    10
+  );
+  PM.addProduct(
+    "Carbon de Quebracho Blanco",
+    350,
+    "./img/CarbonQuebrBlanco.jpg",
+    "cb-QuebBco",
+    25
+  );
+  PM.addProduct("Briquetas", 600, "./img/Briquetas.jpg", "cb-Briq", 15);
+}
